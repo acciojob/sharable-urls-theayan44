b33d54b51ad2414a1f79b1749deec4fcdfd4a685
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-05-28 23:44:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const nameInput = document.getElementById("name");
-const yearInput = document.getElementById("year");
-const headingInput = document.getElementById("heading");
-let txt = "https://localhost:8080/";
-const name = nameInput.value;
-const year = yearInput.value;
-if(name.length != 0 && year.length != 0){
-	txt = txt + "?name=" + name + "&year=" + year;
-}else if(name.length != 0 && year.length == 0){
-	txt = txt + "?name=" + name;
-}else if(name.length == 0 && year.length != 0){
-	txt = txt + "?year=" + year;
+
+function handleClick() {
+	const nameInput = document.getElementById("name");
+	const yearInput = document.getElementById("year");
+	const headingInput = document.getElementById("heading");
+	let txt = "https://localhost:8080/";
+	const name = nameInput.value;
+	const year = yearInput.value;
+	if(name.length != 0 && year.length != 0){
+		txt = txt + "?name=" + name + "&year=" + year;
+	}else if(name.length != 0 && year.length == 0){
+		txt = txt + "?name=" + name;
+	}else if(name.length == 0 && year.length != 0){
+		txt = txt + "?year=" + year;
+	}
+	headingInput.innerText = txt;	
 }
-headingInput.innerText = txt;
